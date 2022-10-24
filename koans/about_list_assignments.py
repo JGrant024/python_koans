5bcc8f2ff3af464e3b5cd3a3d9688241ecd91835
JGrant024 <jonathan.grant23@icloud.com>
2022-10-24 14:21:32 -0400

    completed about_list_assignments

--- koans/about_list_assignments.py
+++ koans/about_list_assignments.py
@@ -10,34 +10,34 @@ from runner.koan import *
 class AboutListAssignments(Koan):
     def test_non_parallel_assignment(self):
         names = ["John", "Smith"]
-        self.assertEqual(__, names)
+        self.assertEqual(['John','Smith'], names)
 
     def test_parallel_assignments(self):
         first_name, last_name = ["John", "Smith"]
-        self.assertEqual(__, first_name)
-        self.assertEqual(__, last_name)
+        self.assertEqual('John', first_name) #Enter first name in string form 
+        self.assertEqual('Smith', last_name) #Enter last name in string form 
 
     def test_parallel_assignments_with_extra_values(self):
         title, *first_names, last_name = ["Sir", "Ricky", "Bobby", "Worthington"]
-        self.assertEqual(__, title)
-        self.assertEqual(__, first_names)
-        self.assertEqual(__, last_name)
+        self.assertEqual("Sir", title) #Enter title such as MR. or Mrs. 
+        self.assertEqual(["Ricky","Bobby"], first_names) #Since there is no middle initial slot put both in first name 
+        self.assertEqual("Worthington", last_name) #Enter last name of list 
 
     def test_parallel_assignments_with_fewer_values(self):
         title, *first_names, last_name = ["Mr", "Bond"]
-        self.assertEqual(__, title)
-        self.assertEqual(__, first_names)
-        self.assertEqual(__, last_name)
+        self.assertEqual("Mr", title) #Title such as mr, mrs
+        self.assertEqual([], first_names) #Since there is no first name i made the list empty 
+        self.assertEqual("Bond", last_name) #Entered last name 
 
     def test_parallel_assignments_with_sublists(self):
         first_name, last_name = [["Willie", "Rae"], "Johnson"]
-        self.assertEqual(__, first_name)
-        self.assertEqual(__, last_name)
+        self.assertEqual(["Willie", "Rae"], first_name) #Since there is no middle initial slot put both in first name 
+        self.assertEqual("Johnson", last_name)
 
     def test_swapping_with_parallel_assignment(self):
         first_name = "Roy"
         last_name = "Rob"
         first_name, last_name = last_name, first_name
-        self.assertEqual(__, first_name)
-        self.assertEqual(__, last_name)
+        self.assertEqual("Rob", first_name) #Swap First name with Last name
+        self.assertEqual("Roy", last_name)  #Swap Last name with First name 
 
