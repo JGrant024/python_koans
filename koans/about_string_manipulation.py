5c8c81a229b7aefd0433dd420fcba47e28e167bb
JGrant024 <jonathan.grant23@icloud.com>
2022-10-25 10:22:43 -0400

    updated about_string_manipulation

--- koans/about_string_manipulation.py
+++ koans/about_string_manipulation.py
@@ -9,38 +9,47 @@ class AboutStringManipulation(Koan):
         value1 = 'one'
         value2 = 2
         string = "The values are {0} and {1}".format(value1, value2)
-        self.assertEqual(__, string)
+        self.assertEqual('The values are one and 2', string)
+
+        # https://www.geeksforgeeks.org/python-string-format-method/
+        # I used the following documentation for the following question below 
 
     def test_formatted_values_can_be_shown_in_any_order_or_be_repeated(self):
         value1 = 'doh'
         value2 = 'DOH'
         string = "The values are {1}, {0}, {0} and {1}!".format(value1, value2)
-        self.assertEqual(__, string)
+        self.assertEqual('The values are DOH, doh, doh and DOH!', string) 
+        #You use the values from 1 and 2 from the string 
 
     def test_any_python_expression_may_be_interpolated(self):
         import math # import a standard python module with math functions
 
+        # I googled the square root of 5 and rounded it four decimal places afterwards 
+        # https://www.google.com/search?q=whats+the+square+root+of+5&oq=whats+the+square+root+of+5&aqs=chrome..69i57j6j0i512l8.5393j1j7&sourceid=chrome&ie=UTF-8
         decimal_places = 4
         string = "The square root of 5 is {0:.{1}f}".format(math.sqrt(5),
             decimal_places)
-        self.assertEqual(__, string)
+        self.assertEqual('The square root of 5 is 2.2361', string)
 
+    #https://www.geeksforgeeks.org/check-string-substring-another/
+    # I used the following documentation for the question below 
     def test_you_can_get_a_substring_from_a_string(self):
         string = "Bacon, lettuce and tomato"
-        self.assertEqual(__, string[7:10])
+        self.assertEqual('let', string[7:10])
 
     def test_you_can_get_a_single_character_from_a_string(self):
         string = "Bacon, lettuce and tomato"
-        self.assertEqual(__, string[1])
+        self.assertEqual('a', string[1])
 
     def test_single_characters_can_be_represented_by_integers(self):
-        self.assertEqual(__, ord('a'))
-        self.assertEqual(__, ord('b') == (ord('a') + 1))
+        self.assertEqual(97, ord('a'))
+        self.assertEqual(True, ord('b') == (ord('a') + 1))
 
+    #Translated words into a string 
     def test_strings_can_be_split(self):
         string = "Sausage Egg Cheese"
         words = string.split()
-        self.assertListEqual([__, __, __], words)
+        self.assertListEqual(["Sausage", "Egg", "Cheese"], words)
 
     def test_strings_can_be_split_with_different_patterns(self):
         import re #import python regular expression library
@@ -50,7 +59,7 @@ class AboutStringManipulation(Koan):
 
         words = pattern.split(string)
 
-        self.assertListEqual([__, __, __, __], words)
+        self.assertListEqual(["the", "rain", "in", "spain"], words)
 
         # Pattern is a Python regular expression pattern which matches ',' or ';'
 
